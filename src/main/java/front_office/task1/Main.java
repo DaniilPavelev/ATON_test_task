6006f48cb15e10ff64676482d4ea03921f059a95
Daniil Paveliev <khiromuelrick@gmail.com>
2023-05-14 21:16:56 +0300

    Had added commentaries

--- src/main/java/front_office/task1/Main.java
+++ src/main/java/front_office/task1/Main.java
@@ -5,24 +5,24 @@ import java.util.Map;
 
 public class Main {
 
-    private static Map<Integer,String> JoeyPhrases = new HashMap<>();
-    private static Map<Integer,String> ChandlerPhrases = new HashMap<>();
-    private static Map<Integer,String> MonicaPhrases = new HashMap<>();
-    private static Map<Integer,String> PhoebePhrases = new HashMap<>();
-    private static Map<Integer,String> RachelPhrases = new HashMap<>();
-    private static Map<Integer,String> RossPhrases = new HashMap<>();
+    private static Map<Integer, String> JoeyPhrases = new HashMap<>();
+    private static Map<Integer, String> ChandlerPhrases = new HashMap<>();
+    private static Map<Integer, String> MonicaPhrases = new HashMap<>();
+    private static Map<Integer, String> PhoebePhrases = new HashMap<>();
+    private static Map<Integer, String> RachelPhrases = new HashMap<>();
+    private static Map<Integer, String> RossPhrases = new HashMap<>();
 
     public static void main(String[] args) {
 
         fillPhrases();
 
         // Создание 6-ти потоков
-        Performansist Joey = new Performansist(JoeyPhrases,"Joey");
+        Performansist Joey = new Performansist(JoeyPhrases, "Joey");
         Performansist Chandler = new Performansist(ChandlerPhrases, "Chandler");
         Performansist Monica = new Performansist(MonicaPhrases, "Monica");
-        Performansist Phoebe = new Performansist(PhoebePhrases,"Phoebe");
-        Performansist Rachel = new Performansist(RachelPhrases,"Rachel");
-        Performansist Ross = new Performansist(RossPhrases,"Ross");
+        Performansist Phoebe = new Performansist(PhoebePhrases, "Phoebe");
+        Performansist Rachel = new Performansist(RachelPhrases, "Rachel");
+        Performansist Ross = new Performansist(RossPhrases, "Ross");
 
         Joey.start();
         Chandler.start();
@@ -32,25 +32,25 @@ public class Main {
         Ross.start();
     }
 
-    /** Заполнение фраз для актёров */
-    private static void fillPhrases(){
-        JoeyPhrases.put(1,"Hey,hey.");
-        ChandlerPhrases.put(2,"Hey.");
-        PhoebePhrases.put(3,"Hey.");
-        ChandlerPhrases.put(4,"And this from cry-for-help department. Are you wearing makeup?");
-        JoeyPhrases.put(5,"Yes, I am. As of today, i am officially Joe Tribbiani, actor slash model.");
-        ChandlerPhrases.put(6,"That's so funny, 'cause I was thinking you look more like Joe Tribbiani, man slash woman.");
-        PhoebePhrases.put(7,"What were you, modeling for?");
-        JoeyPhrases.put(8,"You know those posters for the City Free Clinic?");
-        MonicaPhrases.put(9,"Oh, wow, so you're gonna be one of those \"healthy, healthy, healthy guys\"?");
-        PhoebePhrases.put(10,"You know, the asthma guy was really cute.");
-        ChandlerPhrases.put(11,"Do you know which one you're gonna be?");
+    /**
+     * Заполнение фраз для актёров
+     */
+    private static void fillPhrases() {
+        JoeyPhrases.put(1, "Hey,hey.");
+        ChandlerPhrases.put(2, "Hey.");
+        PhoebePhrases.put(3, "Hey.");
+        ChandlerPhrases.put(4, "And this from cry-for-help department. Are you wearing makeup?");
+        JoeyPhrases.put(5, "Yes, I am. As of today, i am officially Joe Tribbiani, actor slash model.");
+        ChandlerPhrases.put(6, "That's so funny, 'cause I was thinking you look more like Joe Tribbiani, man slash woman.");
+        PhoebePhrases.put(7, "What were you, modeling for?");
+        JoeyPhrases.put(8, "You know those posters for the City Free Clinic?");
+        MonicaPhrases.put(9, "Oh, wow, so you're gonna be one of those \"healthy, healthy, healthy guys\"?");
+        PhoebePhrases.put(10, "You know, the asthma guy was really cute.");
+        ChandlerPhrases.put(11, "Do you know which one you're gonna be?");
         JoeyPhrases.put(12, "No, but you hear lyme disease is open, so...(crosses fingers)");
-        ChandlerPhrases.put(13,"Good luck, man. I hope you get it.");
+        ChandlerPhrases.put(13, "Good luck, man. I hope you get it.");
         JoeyPhrases.put(14, "Thanks.");
     }
-
-
 
 
 }
--- src/main/java/front_office/task1/Performansist.java
+++ src/main/java/front_office/task1/Performansist.java
@@ -4,14 +4,22 @@ import java.util.Map;
 
 public class Performansist extends Thread {
 
-    /** Фразы конкретного актёра с номер в котором он должен их сказать */
+    /**
+     * Фразы конкретного актёра с номер в котором он должен их сказать
+     */
     private Map<Integer, String> phrases;
 
-    /** Счётчик фраз для всех актеров */
+    /**
+     * Счётчик фраз для всех актеров
+     */
     private static volatile int counter = 1;
-    /** Общее количество всех враз в сценке */
+    /**
+     * Общее количество всех враз в сценке
+     */
     private static int phrasesCount;
-    /** Имя актера */
+    /**
+     * Имя актера
+     */
     private String name;
 
     public Performansist(Map<Integer, String> phrases, String name) {
@@ -19,24 +27,20 @@ public class Performansist extends Thread {
         phrasesCount = phrasesCount + phrases.size();
         this.name = name;
     }
-    /** Выписывает в косноль фразу
+    /**
+     * Выписывает в косноль фразу
      *
      * @param n номер фразы в сценке
-     * */
+     */
     private void sayPhrase(Integer n) {
         System.out.println(name + ": " + phrases.get(n));
     }
-    /** Инкрементирует счётчик фраз на 1*/
-    private void incCounter() {
-        counter++;
-    }
-
     @Override
     public void run() {
         while (true) {
             if (phrases.containsKey(counter)) {
                 sayPhrase(counter);
-                incCounter();
+                counter++;
             }
             if (counter > phrasesCount)
                 return;
